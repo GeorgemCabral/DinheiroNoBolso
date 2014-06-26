0af0194da2e770a987b5db039bc1c7d124703fc0
William Silva <williamdouglas@r7.com>
2014-06-26 10:48:53 -0300

    Subindo aplicativo novamente e configurado

---
+++ DinheiroNoBolso/app/src/main/java/williamsilva/dinheironobolso/ListarDespesaActivity.java
@@ -0,0 +1,90 @@
+package williamsilva.dinheironobolso;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+import williamsilva.dinheironobolso.models.Despesa;
+
+import static android.widget.AdapterView.OnItemClickListener;
+
+public class ListarDespesaActivity extends ActionBarActivity {
+
+    private ListView lista;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_despesa);
+
+        lista = (ListView) findViewById(R.id.listaDeDespesas);
+
+        registerForContextMenu(lista);
+
+        lista.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //escreva o codigo para click simples aqui:
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //escreva o codigo para click longo aqui:
+
+                return false;
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.listar_despesa, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Despesa despesa = new Despesa(this);
+        List<Despesa> despesas = despesa.getLista();
+        int layout = android.R.layout.simple_list_item_1;
+        ArrayAdapter<Despesa> adapter = new ArrayAdapter<Despesa>(this,layout,despesas);
+        lista.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        getMenuInflater().inflate(R.menu.opcoes,menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+}
---
+++ DinheiroNoBolso/app/src/main/java/williamsilva/dinheironobolso/NovaReceitaActivity.java
@@ -0,0 +1,97 @@
+package williamsilva.dinheironobolso;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class NovaReceitaActivity extends ActionBarActivity {
+
+    private int ano,mes,dia;
+    private Button dataRec;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_receita);
+
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        //tratando o campo dataRec
+        Calendar calendario = Calendar.getInstance();
+        ano = calendario.get(Calendar.YEAR);
+        mes = calendario.get(Calendar.MONTH);
+        dia = calendario.get(Calendar.DAY_OF_MONTH);
+        dataRec = (Button) findViewById(R.id.dataReceb);
+        dataRec.setText(dia + "/" + (mes+1) + "/" + ano);
+        //fim do tratamento
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nova_receita, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void selecionarData(View view)
+    {
+        showDialog(view.getId());
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if(R.id.dataReceb == id)
+        {
+            return new DatePickerDialog(this, listener, ano, mes, dia);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener listener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            ano = year;
+            mes = monthOfYear;
+            dia = dayOfMonth;
+            dataRec.setText(dia + "/" + (mes+1) + "/" + ano);
+
+        }
+    };
+
+    public void salvarReceita(View view)
+    {
+        teste("Testando o botão");
+    }
+
+    private void teste (String msg)
+    {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show ();
+    }
+}
